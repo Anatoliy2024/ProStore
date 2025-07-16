5e26dbb194b16fcd96399f8a91829a7c1d0f7a82
Anatoliy2024 <gredlend20@gmail.com>
2025-07-16 13:56:26 +0300

    ass timeZone

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,9 @@ export const dynamic = "force-static"
 
 export default async function About() {
   // Время генерации страницы
-  const generatedAt = new Date().toLocaleString("ru-RU")
+  const generatedAt = new Date().toLocaleString("ru-RU", {
+    timeZone: "Europe/Moscow",
+  })
   return (
     <div className="min-h-[calc(100vh-50px)]  flex justify-center items-center flex-col gap-[20px]">
       <h1 className="text-3xl">О нас</h1>
